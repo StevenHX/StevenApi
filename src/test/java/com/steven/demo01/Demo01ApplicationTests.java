4d614106fcb4b3acae0a8fca08ef6208c8e0fe63
wingtech\huanxiao <18122147149@qq.com>
2022-06-29 15:50:05 +0800

    添加发送邮件

--- src/test/java/com/steven/demo01/Demo01ApplicationTests.java
+++ src/test/java/com/steven/demo01/Demo01ApplicationTests.java
@@ -1,13 +1,36 @@
 package com.steven.demo01;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootTest
 class Demo01ApplicationTests {
-
+    @Autowired
+    private JavaMailSender javaMailSender;
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testSendEmail() {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom("datongganjin@163.com");
+        msg.setBcc();
+        msg.setTo("1812247149@qq.com");
+        msg.setSubject("StevenApi投稿");
+        msg.setText("您在"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())+"登录该系统");
+        try {
+            javaMailSender.send(msg);
+        } catch (MailException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
 }
